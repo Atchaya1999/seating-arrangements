eaed8fb43bb54206f58b1a36bf8cf9fba2c14b64
Atchaya1999 <36922037+Atchaya1999@users.noreply.github.com>
2018-06-08 20:38:16 +0530

    Create seatingarrangement.cpp

---
+++ seatingarrangement.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+class seat
+{   public:
+    int m;
+    char *q;
+    public:
+    seat()
+    {
+        m=0;
+      q= new char[m+1];
+        
+    }
+};
+int main()
+{   int A[]={6,18,30,42,54,66,78,90,102};
+    int num,count,p;
+    cin>>num; 
+    seat *S[num];
+    for(int i=0;i<num;i++){
+     S[i]=new seat;
+      cin>>S[i]->m;
+    }
+   
+for(int i=0;i<num;i++){
+ 
+    for(int j=0;j<=8;j++)
+    { p=A[j];
+        if((S[i]->m>=(p-5))&&(S[i]->m<=(p+6)))
+        {
+            count=0;
+            for(int k=(p-5);k<=(p+6);k++)
+            {   count++;
+                if(S[i]->m==k)
+                {
+                    switch(count)
+                    {
+                        case(1):{S[i]->m+=11; S[i]->q=" WS";goto Akshaya;}
+                        case(2):{S[i]->m+=9; S[i]->q=" MS";goto Akshaya;}
+                        case(3):{S[i]->m+=7; S[i]->q=" AS";goto Akshaya;}
+                        case(4):{S[i]->m+=5; S[i]->q=" AS";goto Akshaya;}
+                        case(5):{S[i]->m+=3; S[i]->q=" MS"; goto Akshaya;}
+                        case(6):{S[i]->m+=1; S[i]->q=" WS";goto Akshaya;}
+                        case(7):{S[i]->m-=1; S[i]->q=" WS";goto Akshaya;}
+                        case(8):{S[i]->m-=3; S[i]->q=" MS";goto Akshaya;}
+                        case(9):{S[i]->m-=5; S[i]->q=" AS";goto Akshaya;}
+                        case(10):{S[i]->m-=7; S[i]->q=" AS";goto Akshaya;}
+                        case(11):{S[i]->m-=9; S[i]->q=" MS";goto Akshaya;}
+                        case(12):{S[i]->m-=11; S[i]->q=" WS";goto Akshaya;}
+                    }
+                   goto Akshaya; 
+                }
+                
+            }
+        }
+    }
+   Akshaya: count=0; //nothing
+}
+for(int l=0;l<num;l++)
+{
+cout<<S[l]->m<<S[l]->q<<endl;
+}
+return 0;
+}
